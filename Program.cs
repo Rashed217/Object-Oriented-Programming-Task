ccb3f36cf85c96eaa15af4935fac5879a4887a7d
Rashed Al Ghailani <Authority217@gmail.com>
2024-09-25 15:58:48 +0400

    Added some accounts, added the accounts to the bank, added DisplayAllAccount() and GetAccountByNumber()

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bank bank = new Bank();
+
+            // Creating random accounts
+            BankAccount account1 = new BankAccount ("238518", "Rashed", 2808.00m);
+            BankAccount account2 = new BankAccount ("823541", "Riimz", 1234.32m);
+
+
+            // Add the account to the bank
+            bank.AddAccount (account1);
+            bank.AddAccount (account2);
+
+
+            // Display all accounts
+            bank.DisplayAllAccount();
+
+
+            // Get a specific account
+            var retrievedAccount = bank.GetAccountByNumber("123456");
+            if (retrievedAccount != null)
+            {
+                Console.WriteLine($"Retrieved Account: {retrievedAccount}");
+            }
+            else
+            {
+                Console.WriteLine("Account not found.");
+            }
         }
     }
 }
